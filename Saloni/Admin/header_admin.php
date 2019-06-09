abb9c699107d819993c96f4e6f7e97c8bab56f95
SaloniKhatu <50865198+SaloniKhatu@users.noreply.github.com>
2019-06-09 13:58:36 +0530

    Add files via upload

---
+++ Saloni/Admin/header_admin.php
@@ -0,0 +1,59 @@
+<header class="topbar">
+            <nav class="navbar top-navbar navbar-toggleable-sm navbar-light">
+                <!-- ============================================================== -->
+                <!-- Logo -->
+                <!-- ============================================================== -->
+                
+                <div class="navbar-collapse">
+                    <!-- ============================================================== -->
+                    <!-- toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <ul class="navbar-nav mr-auto mt-md-0">
+                        <!-- This is  -->
+                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
+                        <li class="nav-item"> <a class="nav-link sidebartoggler hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                        <!-- ============================================================== -->
+                        <!-- Search -->
+                        <!-- ============================================================== -->
+                       
+                        <!-- ============================================================== -->
+                        <!-- End Messages -->
+                        <!-- ============================================================== -->
+                        
+                        <!-- ============================================================== -->
+                        <!-- Profile -->
+                        <!-- ============================================================== -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="https://media.licdn.com/dms/image/C5103AQGRRrVD0xWJDA/profile-displayphoto-shrink_200_200/0?e=1564617600&v=beta&t=zJbBnIy9oeemLrPbPRl3PPoDuhz1QQnm4DFjBkLh0yg" alt="user" class="profile-pic" /></a>
+                            <div class="dropdown-menu dropdown-menu-right scale-up">
+                                <ul class="dropdown-user">
+                                    <li>
+                                        <div class="dw-user-box">
+                                            <div class="u-img"><img src="https://media.licdn.com/dms/image/C5103AQGRRrVD0xWJDA/profile-displayphoto-shrink_200_200/0?e=1564617600&v=beta&t=zJbBnIy9oeemLrPbPRl3PPoDuhz1QQnm4DFjBkLh0yg" alt="user"></div>
+                                            <div class="u-text">
+                                                <h4>Saloni Khatu</h4>
+                                                <p class="text-muted">salonikhatu123@gmail.com</p><a href="profile.html" class="btn btn-rounded btn-danger btn-sm">View Profile</a></div>
+                                        </div>
+                                    </li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#"><i class="ti-user"></i> My Profile</a></li>
+                                    <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li>
+                                    <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <!-- ============================================================== -->
+                        <!-- Language -->
+                        <!-- ============================================================== -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="flag-icon flag-icon-in"></i></a>
+                            <div class="dropdown-menu dropdown-menu-right scale-up"> <a class="dropdown-item" href="#"><i class="flag-icon flag-icon-in"></i> India</a> <a class="dropdown-item" href="#"><i class="flag-icon flag-icon-fr"></i> French</a> <a class="dropdown-item" href="#"><i class="flag-icon flag-icon-cn"></i> China</a> <a class="dropdown-item" href="#"><i class="flag-icon flag-icon-de"></i> Dutch</a> </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
---
+++ Saloni/Admin/sidebar_admin.php
@@ -0,0 +1,107 @@
+<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- User profile -->
+                <div class="user-profile" style="background: url(../assets/images/background/user-info.jpg) no-repeat;">
+                    <!-- User profile image -->
+                    <div class="profile-img"> <img src="hello.jpg" width="50" height="50"  alt="user" /> </div>
+                    <!-- User profile text-->
+                    <div class="profile-text"> <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Saloni Khatu</a>
+                        <div class="dropdown-menu animated flipInY">
+                            <a href="#" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
+                            <a href="#" class="dropdown-item"><i class="ti-wallet"></i> My Balance</a>
+                            <a href="#" class="dropdown-item"><i class="ti-email"></i> Inbox</a>
+                            <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+                            <div class="dropdown-divider"></div> <a href="login.html" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- End User profile text-->
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-small-cap">PERSONAL</li>
+                        <li>
+                            <a class="waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard</span></a>
+                            <li>
+                            <a class=" has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-chair-school"></i><span class="hide-menu">School</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>View/Edit</li><br>
+                                <li>Add</li><br>
+                                <li>Moderate</li><br>
+                                <li>Support</li><br>
+                                <li>Boards</li><br>
+                                <li>Facilities</li><br>
+                                <li>Add Field</li><br>
+                            </ul>
+                        </ul>
+                        </li>
+                            <li>
+                            <a class="has-arrow waves-effect waves-dark" href="admin_parent.php" aria-expanded="false"><i class="mdi mdi-account-multiple-plus"></i><span class="hide-menu">Parent</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>View/Edit</li><br>
+                                <li>Moderations</li><br>
+                            </ul>
+                        </ul>
+                        </li>
+                            
+                        </li>
+                        <li> 
+                            <a class="has-arrow waves-effect waves-dark" href="admin_child.php" aria-expanded="false"><i class="mdi mdi-human-child"></i><span class="hide-menu">Child</span></a>
+                             <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>View/Edit</li><br>
+                                <li>Moderations</li><br>
+                            </ul>
+                            </ul>
+                        </li>
+                        <li>
+                        <a class="has-arrow waves-effect waves-dark" href="admin_child.php" aria-expanded="false"><i class="mdi mdi-account-settings-variant"></i><span class="hide-menu">Support</span></a>
+                             <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>School</li><br>
+                                <li>Parent</li><br>
+                                <li>Child</li><br>
+                            </ul>
+                        </ul>
+                        </li>
+                        <li>
+                        <a class="has-arrow waves-effect waves-dark" href="admin_child.php" aria-expanded="false"><i class="mdi mdi-comment-account"></i><span class="hide-menu">FAQ</span></a>
+                             <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>View/Edit</li><br>
+                                <li>Add</li><br>
+                            </ul>
+                        </ul>
+                        </li>
+                        <li>
+                        <a class="has-arrow waves-effect waves-dark" href="admin_child.php" aria-expanded="false"><i class="mdi mdi-account-multiple-outline"></i><span class="hide-menu">Team Members</span></a>
+                             <ul aria-expanded="false" class="collapse">
+                                <ul>
+                                <li>View/Edit</li><br>
+                                <li>Add</li><br>
+                            </ul>
+                        </ul>
+                        </li>
+
+
+                        
+                        
+                        
+                        
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <!-- Bottom points-->
+            <div class="sidebar-footer">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Settings"><i class="ti-settings"></i></a>
+                <!-- item-->
+                <a href="mailto:salonikhatu123@gmail.com" class="link" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+            </div>
+            <!-- End Bottom points-->
+        </aside>
